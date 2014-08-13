b934581e34127f438b7c3814f4f1f9c44c6bf9c1
alfski <alfski@gmail.com>
2014-08-13 20:04:23 +1000

    rename lib to viewsynceffect

--- README.md
+++ README.md
@@ -2,18 +2,26 @@
 
 Based on StereoEffect.js from vr.chromeexperiments.com.
 
+Shares the camera position and quaternion to a websocket relayer.
+
+Wraps the camera in the ThreeJS app, with a camera that is setable from a websocket and with particular fov and yaw offset.
+
+`<script src="ViewSyncEffect.js"></script>`
+
+##How to use/implement these demo's
+
+Place ViewSyncEffect.js, lglion.html and lgpompei.html into the /examples/ folder in the potree distribution
+
 Change websocket server from "ws://192.168.0.233:3000/relay" to the URI of your websocket relay/echo service.
+Any simple websocket echo server will do the job. I have one in Perl... just 'coz I'm oldskool.
 
-Copy ViewSyncEffect.js, lglions.html and lgpompei.html into the /examples/ folder in the potree distribution
-
-
-##How to start the apps
+##How to launch the apps
 
 ```
-Master http://YOURWEBSERVER/lglion.html?master
+Master http://YOURWEBSERVER/lglion.html
 Slaves http://YOURWEBSERVER/lglion.html?slave&fov=30&yaw=33
 
-Master http://YOURWEBSERVER/lgpompei.html?master
+Master http://YOURWEBSERVER/lgpompei.html
 Slaves http://YOURWEBSERVER/lgpompei.html?slave&fov=25&yaw=-29
 ```
 
@@ -21,6 +29,17 @@ Slaves http://YOURWEBSERVER/lgpompei.html?slave&fov=25&yaw=-29
 
 "slave" enables camera sync.
 
-"fov" to set the camera horizontal field of view. Also works on master.
+"fov" to set the camera horizontal field of view, in degrees. This parameter can also be used to set the master fov for display purposes.
 
 "yaw" set the slave camera yawOffset, in degrees.
+
+##Issues & To Do's
+
+* possibly add pitch and roll offseting, just for 'ViewSync' completeness.
+
+* I think there's cleanups to when the camera needs to be set.
+
+* smarter relayer, that resends current camera pov.
+
+* look at websocket-enabling "dat.gui.js"
+
--- ViewSyncEffect.js
+++ ViewSyncEffect.js
@@ -15,8 +15,6 @@ THREE.ViewSyncEffect = function ( renderer ) {
 
 	var _websocket = new WebSocket( "ws://192.168.0.233:3000/relay" ); // arg?
 
-	var _yawRads = 0.0; // Alf - yaw rotation offset in radians
-
 	var _position = new THREE.Vector3();
 	var _quaternion = new THREE.Quaternion();
 	var _scale = new THREE.Vector3(); // needed but not used
@@ -25,10 +23,14 @@ THREE.ViewSyncEffect = function ( renderer ) {
 	var _rollAxis = new THREE.Vector3( 0,0,1 ); // roll - rotate around Z ?check?
 	var _pitchAxis = new THREE.Vector3( 1,0,0 ); // pitch - rotate around X ?check?
 
+	var _yawRads = 0.0; // yaw rotation offset in radians
+	var _pitchRads = 0.0; // not used yet
+	var _rollRads = 0.0; // not used yet
+
 	// initialization
 	var _wsConnected = false;
 	var _slave = false;
-	var _yaw = 0.0;
+	var _yaw = 0.0; var _pitch = 0.0; var _roll = 0.0;
 	var _fov = 0.0;
 	var _lastMesg = "";
 
@@ -40,7 +42,7 @@ THREE.ViewSyncEffect = function ( renderer ) {
 		_yaw = _qryArgs[ "yaw" ];
 		console.log( "_yaw:"+_yaw );
 	}
-	if ( _qryArgs[ "pitch" ] ) { } // also do pitch/roll at some point
+	if ( _qryArgs[ "pitch" ] ) { } // eventually also do pitch/roll
 	if ( _qryArgs[ "roll" ] ) { }
 
 	if ( _qryArgs[ "fov" ] ) {
@@ -48,13 +50,14 @@ THREE.ViewSyncEffect = function ( renderer ) {
 		console.log( "_fov:"+_fov );
 	}
 
+	// set up websocket callbacks
 	_websocket.onopen = function () {
 		_wsConnected = true;
-		// ask relay to resend last pov message
+		// now ask relay to resend last pov message?
 	}
 	_websocket.onclose = function () { _wsConnected = false; }
 
-	if (_slave) { // only slaves respond to inbound ws messages
+	if (_slave) { // only slaves need this
 		_websocket.onmessage = function ( evt ) {
 			//console.log("evt:"+evt.data);
 			var camData = JSON.parse( evt.data );
@@ -78,11 +81,13 @@ THREE.ViewSyncEffect = function ( renderer ) {
 
 		if ( camera.parent === undefined ) camera.updateMatrixWorld();
 	
-		if ( !_slave ) { // get & send camera position & quaternion via ws
+		if ( !_slave ) { // get & send camera position & quaternion via websocket
+
 			camera.matrixWorld.decompose( _position, _quaternion, _scale );
 			var pov = { p:_position, q:_quaternion };
                         var povMesg = JSON.stringify( pov );
 			//console.log("pov:"+povMesg);
+
 			if ( povMesg != _lastMesg && _wsConnected ) { // only if new data and connected
                         	_websocket.send( povMesg ); // needs "pov" prefix
 				_lastMesg = povMesg;
@@ -94,13 +99,13 @@ THREE.ViewSyncEffect = function ( renderer ) {
 		camera.updateProjectionMatrix();
 
 		cameraViewSync.projectionMatrix = camera.projectionMatrix;
-		cameraViewSync.position.copy( _position ); // for slave can we do these in onmessage?
+		cameraViewSync.position.copy( _position ); // for slave can we do these in ws.onmessage?
 		cameraViewSync.quaternion.copy( _quaternion );
 
 		if (_yawRads != 0 ) {
 			cameraViewSync.rotateOnAxis( _yawAxis, _yawRads );
 		}
-		// maybe add pitch & roll?
+		// eventually add pitch & roll
 
 		cameraViewSync.updateMatrixWorld();
 
